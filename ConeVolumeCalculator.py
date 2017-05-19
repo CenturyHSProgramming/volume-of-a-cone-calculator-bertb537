61bb0c983c0f7848bcf872bfd3a2d2119154c917
bertb537 <brett.bertrand@frontier.com>
2017-05-19 08:54:50 -0700

    Add files via upload

--- ConeVolumeCalculator.py
+++ ConeVolumeCalculator.py
@@ -3,12 +3,19 @@
 # it **calculateConeVolume()** that calculates the volume of a cone
 # factor based on the Volume Calculator
 # Calculator.net (http://www.calculator.net/volume-calculator.html)
-
+import math
 # Define Function below
 # be sure to return an integer
-
+def calculateConeVolume(r, h) :
+    volume = 1/3*math.pi*r**2*h
+    volume = round(volume, 2)
+    return volume
 
 if __name__ == '__main__':
-    # Call the function in here if you want to test it
-    # Make sure it's indented
-    pass # remove or comment out this line if you wish to test the function
+    print("Hello user.")
+    radius = float(input("What is the radius of your cone?"))
+    height = float(input("What is the height of your cone?"))
+    volume = calculateConeVolume(radius, height)
+    volume = round(volume, 2)
+    
+    print("The volume of your cone is " + str(volume))
